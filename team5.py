84a3d2d326cbcdd55eb9200c65d8c3cfc0d7ddfd
MrLuuk <34381067+MrLuuk@users.noreply.github.com>
2018-01-17 13:05:40 -0500

    Add files via upload

---
+++ team5.py
@@ -0,0 +1,18 @@
+####
+# Each team's file must define four tokens:
+#     team_name: a string
+#     strategy_name: a string
+#     strategy_description: a string
+#     move: A function that returns 'c' or 'b'
+####
+
+team_name = 'Scientific Calculator' # Only 10 chars displayed.
+strategy_name = 'random'
+strategy_description = 'randomized output'
+    
+def move(my_history, their_history, my_score, their_score):
+    import random
+    possibleMove = ['b','c']
+    return random.choice(possibleMove)
+    print random.choice(possibleMove)
+   
