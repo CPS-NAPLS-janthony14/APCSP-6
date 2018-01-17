db92db955ede4dd3221c9061348358875a4fe475
MrLuuk <34381067+MrLuuk@users.noreply.github.com>
2018-01-17 13:06:55 -0500

    Update team5.py

--- team5.py
+++ team5.py
@@ -14,5 +14,3 @@ def move(my_history, their_history, my_score, their_score):
     import random
     possibleMove = ['b','c']
     return random.choice(possibleMove)
-    print random.choice(possibleMove)
-   
