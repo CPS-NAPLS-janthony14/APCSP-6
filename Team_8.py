7c85f097699aa4e1c603a8a8cfd63b8df3590fa8
eagleboyna <34380971+eagleboyna@users.noreply.github.com>
2018-01-17 13:22:49 -0500

    Add files via upload

---
+++ Team_8.py
@@ -0,0 +1,8 @@
+#Team 8: Passion
+#Noah Malik, George Brothers 
+#1/17/18
+#Team_8
+#Purpose: to write code for the Prisoner's Dilemma
+
+def move(my_history, their_history, my_score, their_score):
+    return 'b'
